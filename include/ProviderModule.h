f6ac345c2bda079e12d0e3d42edb325259f3c916
Thomas <tnp94@yahoo.com>
2020-02-19 17:50:03 -0800

    Update ProviderModule.h

--- include/ProviderModule.h
+++ include/ProviderModule.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Service.h"
 
 class ProviderModule{
@@ -12,3 +13,25 @@ class ProviderModule{
       ProviderDirectory directory;
       uint provider_id;
 }
+=======
+#ifndef PROVIDER_MODULE
+#define PROVIDER_MODULE
+
+#include "Service.h"
+#include "ProviderDirectory.h"
+
+class ProviderModule{
+  public:
+      int init(uint providerid);
+      int validate_member(uint memberid);
+      int provide_service(Service& service);
+      int get_provider_directory();
+      ProviderModule();
+      ~ProviderModule();
+  private:
+      ProviderDirectory directory;
+      uint provider_id;
+};
+
+#endif
+>>>>>>> 653d4fedbe6861cab4c7a488d94e73491de48d52
