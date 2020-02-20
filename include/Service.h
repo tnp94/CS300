87f309854e2a5949ee8a5efaf5bee41a179cad48
aujxn <ajn6@pdx.edu>
2020-02-19 17:06:50 -0800

    added service object, removed files for report objects

---
+++ include/Service.h
@@ -0,0 +1,31 @@
+#include <ctime>
+#include <string>
+
+class Service {
+   public:
+      uint get_member_id() const;
+      uint get_provider_id() const;
+      uint get_service_code() const;
+      time_t get_date_added() const;
+      time_t get_service_date() const;
+      std::string get_comments() const;
+
+      Service(
+            const uint member_id,
+            const uint provider_id,
+            const time_t date_added,
+            const time_t service_date,
+            const uint service_code,
+            const std::string comments
+            );
+
+      ~Service();
+
+   private:
+      uint member_id;
+      uint provider_id;
+      time_t date_added;
+      time_t service_date;
+      uint service_code;
+      std::string comments;
+};
---
+++ src/Service.cpp
@@ -0,0 +1,32 @@
+#include "../include/Service.h"
+
+uint get_member_id() {
+   
+   return 0;
+}
+
+uint get_provider_id() {
+
+   return 0;
+}
+
+uint get_service_code() {
+
+   return 0;
+}
+
+time_t get_date_added() {
+
+   return 0;
+}
+
+time_t get_service_date() {
+
+   return 0;
+}
+
+std::string get_comments() {
+
+   std::string a_string;
+   return a_string;
+}
