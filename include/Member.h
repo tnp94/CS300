9a2192166d2763b44eaf3ea10622f8e5c24243c9
mkay1995 <59936508+mkay1995@users.noreply.github.com>
2020-02-19 16:59:00 -0800

    Update Member.h

---
+++ include/Member.h
@@ -0,0 +1,19 @@
+#include "person"
+#include <string>
+
+class Member:public Person{
+ public:
+  std::
+bool suspended;
+}
+
+member(){
+member.name=st::;
+member.address=;
+member.city=;
+member.state=;
+member.zip=;
+member.id=;
+member.suspended= false;
+}
+
