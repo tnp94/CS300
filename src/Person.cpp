e9309f6963003e31091f255b402db1036e60538b
WinnieThePooln <59946459+WinnieThePooln@users.noreply.github.com>
2020-02-23 17:44:54 -0800

    Update Person.cpp

--- src/Person.cpp
+++ src/Person.cpp
@@ -2,7 +2,11 @@
 
 Person::Person(const std::string name, const uint member_id, const std::string city, const std::string state, const uint zip)
 {
-
+    this->name=name;
+    this->member_id=member_id;
+    this->city=city;
+    this->state=state;
+    this->zip=zip;
 }
 
 Person::~Person()
