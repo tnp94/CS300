17bc54844066dc842e17efc452792c4e7403a0c9
WinnieThePooln <59946459+WinnieThePooln@users.noreply.github.com>
2020-02-23 17:59:03 -0800

    Update Member.cpp

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,5 +1,13 @@
 #include "../include/Member.h"
 
+void Member::set_suspended(bool suspended){
+  this->suspended=suspended;
+}
+
+bool Member::get_suspended(){
+  return this->suspended;
+}
+
 Member::Member(const std::string name, const uint member_id, const std::string city, const std::string state, const uint zip):Person(name, member_id, city, state, zip)
 {
   //Person(const std::string name, const uint member_id, const std::string city, const std::string state, const uint zip)
