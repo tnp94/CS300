5a1f73321fbf9febda2841f9e0fd00c7644a0e7d
WinnieThePooln <59946459+WinnieThePooln@users.noreply.github.com>
2020-02-24 15:27:10 -0800

    Update display_info();

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,11 +1,21 @@
 #include "../include/Member.h"
-
+#include<iostream>
+using namespace std;
 void Member::set_suspended(bool suspended){
   this->suspended=suspended;
 }
 
 bool Member::get_suspended(){
   return this->suspended;
+}
+
+void Member::display_info(){
+  cout<<"The member's information:"<<endl;
+  cout<<"Name: "<<get_name()<<endl;
+  cout<<"Member_id: "<<get_id()<<endl;
+  cout<<"City: "<<get_city()<<endl;
+  cout<<"State: "<<get_state()<<endl;
+  cout<<"Zip: "<<get_zip()<<endl;
 }
 
 Member::Member(const std::string name, const uint member_id, const std::string city, const std::string state, const uint zip):Person(name, member_id, city, state, zip)
