a8c4c11bd101757de5b6ee268738a9872406f70c
WinnieThePooln <59946459+WinnieThePooln@users.noreply.github.com>
2020-02-23 18:06:32 -0800

    Update Person.cpp

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,9 +1,49 @@
 #include "../include/Person.h"
 
-Person::Person(const std::string name, const uint member_id, const std::string city, const std::string state, const uint zip)
+std::string Person::get_name(){
+    returen this->name;
+}
+
+std::string Person::get_city(){
+    return this->city;
+}
+
+std::string Person::get_state(){
+     return this->state:
+ }
+
+uint Person::get_zip(){
+    return this->zip;
+}
+
+uint Person::get_id(){
+    return this->id;
+}
+
+void Person::set_name(std::string name){
+    this->name=name;
+}
+
+void Person::set_id(uint id){
+    this->id=id;
+}
+
+void Person::set_city(std::string city){
+    this->city=city;
+}
+
+void Person::set_state(std::string state){
+    this->state=state;
+}
+
+void Person::set_zip(uint zip){
+    this->zip=zip;
+}
+
+Person::Person(const std::string name, const uint id, const std::string city, const std::string state, const uint zip)
 {
     this->name=name;
-    this->member_id=member_id;
+    this->id=id;
     this->city=city;
     this->state=state;
     this->zip=zip;
