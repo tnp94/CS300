f461f2946e806d581a0e149cb6356b22cd93ffba
WinnieThePooln <59946459+WinnieThePooln@users.noreply.github.com>
2020-02-23 17:16:06 -0800

    Update Service.h

--- include/Service.h
+++ include/Service.h
@@ -12,7 +12,12 @@ class Service {
       time_t get_date_added() const;
       time_t get_service_date() const;
       std::string get_comments() const;
-
+      void set_member_id(uint member_id);
+      void set_provider_id(uint provider_id);
+      void set_service_code(uint service_code);
+      void set_data_added(time_t date_added);
+      void set_service_data(time_t service_date);
+      void set_comments(std::string comments);
       Service(
             const uint member_id,
             const uint provider_id,
