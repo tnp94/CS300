5c285914b2d84dd437978759c961b742f2bb1c9d
WinnieThePooln <59946459+WinnieThePooln@users.noreply.github.com>
2020-02-23 17:54:55 -0800

    Update Service.cpp

--- src/Service.cpp
+++ src/Service.cpp
@@ -24,7 +24,31 @@ std::string Service::get_comments() const {
    return this->comments;
 }
 
-Service::Service(const uint member_id, const uint provider_id, const time_t date_added, const time_t service_date, const uint service_code, const std::string comment)
+void Service::set_member_id(uint member_id){
+   this->member_id=member_id;
+}
+
+void Service::set_provider_id(uint provider_id){
+   this->provider_id=provider_id;
+}
+
+void Service::set_date_added(time_t date_added){
+   this->date_added=date_added;
+}
+
+void Service::set_service_date(time_t service_date){
+   this->service_date=service_date;
+}
+
+void Service::set_service_code(uint service_code){
+   this->service_code=service_code;
+}
+
+void Service::set_comments(std::string comments){
+   this->comments=comments;
+}
+
+Service::Service(const uint member_id, const uint provider_id, const time_t date_added, const time_t service_date, const uint service_code, const std::string comments)
 {
    this->member_id=member_id;
    this->provider_id=provider_id;
