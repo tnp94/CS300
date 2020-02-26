c1849073e37b219cf42399a09f5292d024e057dd
mkay1995 <59936508+mkay1995@users.noreply.github.com>
2020-02-26 13:07:32 -0800

    Update Provider.cpp

--- src/Provider.cpp
+++ src/Provider.cpp
@@ -2,14 +2,14 @@
 #include<iostream>
 using namespace std;
 
-void Provider::display_info(){
+/*void Provider::display_info(){
   cout<<"The provider's information:"<<endl;
   cout<<"Name: "<<get_name()<<endl;
   cout<<"Provider_id: "<<get_id()<<endl;
   cout<<"City: "<<get_city()<<endl;
   cout<<"State: "<<get_state()<<endl;
   cout<<"Zip: "<<get_zip()<<endl;
-}
+}*/
 
 
 Provider::Provider(const std::string name, const uint id, const std::string city, const std::string state, const uint zip) : Person(name, id, city, state, zip)
