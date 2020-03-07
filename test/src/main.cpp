a8bf844e62bee635c36f4d25fa2cc2b27cafdfa7
malker97 <56330994+malker97@users.noreply.github.com>
2020-03-07 14:57:23 -0800

    Update main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,3 +1,11 @@
+#include "../include/ProviderModule.h"
+#include "../include/ManagerModule.h"
+#include "../include/InteractiveModule.h"
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
 int main() {
    return 0;
 }
