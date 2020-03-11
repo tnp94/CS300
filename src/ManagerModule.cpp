03cb8bc7bf79ffb4d7966e8925b57e1bc639b478
Xingjian Wang <xingjian@pdx.edu>
2020-03-10 21:56:50 -0700

    Added check that weekly reports have been submitted so that reports are generated only once a week.

--- src/ManagerModule.cpp
+++ src/ManagerModule.cpp
@@ -58,91 +58,99 @@ int ManagerModule::weekly_report() {
     time_t curr_time;
     struct tm * curr_timep;
     time(&curr_time);
+    bool submitted = false;
     cout<<"The system's time is:"<<endl;
     printf("%s", ctime(&curr_time));
     curr_timep=localtime(&curr_time);
+    if (weekday[curr_timep->tm_wday] != "Fri")      //Reset submitted
+        submitted = false;
     cout<<"Today is: "<< weekday[curr_timep->tm_wday] << endl;
-    if( strcmp(weekday[curr_timep->tm_wday],"Fri") == 0){
-      //TODO: Check if weekly report has been run today already by attempting to open the projected file name
-      // If it opens (inFile.isopen()) then we have already run it today and don't need to run it again.
-        cout<<"Today is Friday, so the system will display this week's service."<<endl;
-        time_t tem_time;
-        struct tm* tem_timep;
-        map<time_t ,Service>::iterator iterator;
-        iterator = services.begin();
-        cout<<"All service in this week will be listed: "<<endl;
-        while(iterator!=services.end()){
-            tem_time=iterator->second.get_service_date();
-            tem_timep=localtime(&tem_time);
-            if(curr_timep->tm_mday>7) {//this week all in a month
-                if ((curr_timep->tm_year == tem_timep->tm_year) && (curr_timep->tm_mon == tem_timep->tm_mon) &&
-                    (curr_timep->tm_mday - tem_timep->tm_mday <= 7)) {
-                    cout << "The service's information: " << endl;
-                    iterator->second.display_info();
-                }
-            }
-            else{// this week devided into two months
-                    int cur_days=curr_timep->tm_mday;
-                    int last_days=8-cur_days;
-                    int last_mon_days=0;
-                    int year=1900+curr_timep->tm_year;
-                    switch(curr_timep->tm_mon+1) {
-                        case 1:
-                            last_mon_days=31;
-                            break;
-                        case 2:
-                            if(year%400 == 0 || (year %4 == 0 && year %100 != 0)){
-                                last_mon_days=29;
-                                }
-                            else
-                                last_days=28;
-                            break;
-                        case 3:
-                            last_mon_days=31;
-                            break;
-                        case 4:
-                            last_mon_days=30;
-                            break;
-                        case 5:
-                            last_mon_days=31;
-                        case 6:
-                            last_mon_days=30;
-                            break;
-                        case 7:
-                            last_mon_days=31;
-                            break;
-                        case 8:
-                            last_mon_days=31;
-                            break;
-                        case 9:
-                            last_mon_days=30;
-                            break;
-                        case 10:
-                            last_mon_days=31;
-                            break;
-                        case 11:
-                            last_mon_days=30;
-                            break;
-                        case 12:
-                            last_mon_days=31;
-                            break;
-                    }
-                    if(tem_timep->tm_mon==curr_timep->tm_mon&&tem_timep->tm_mday<=cur_days){
+    if (!submitted) {
+        if (strcmp(weekday[curr_timep->tm_wday], "Fri") == 0) {
+            //TODO: Check if weekly report has been run today already by attempting to open the projected file name
+            // If it opens (inFile.isopen()) then we have already run it today and don't need to run it again.
+            cout << "Today is Friday, so the system will display this week's service." << endl;
+            time_t tem_time;
+            struct tm* tem_timep;
+            map<time_t, Service>::iterator iterator;
+            iterator = services.begin();
+            cout << "All service in this week will be listed: " << endl;
+            while (iterator != services.end()) {
+                tem_time = iterator->second.get_service_date();
+                tem_timep = localtime(&tem_time);
+                if (curr_timep->tm_mday > 7) {//this week all in a month
+                    if ((curr_timep->tm_year == tem_timep->tm_year) && (curr_timep->tm_mon == tem_timep->tm_mon) &&
+                        (curr_timep->tm_mday - tem_timep->tm_mday <= 7)) {
                         cout << "The service's information: " << endl;
                         iterator->second.display_info();
                     }
-                    else if(tem_timep->tm_mon==curr_timep->tm_mon-1&&tem_timep->tm_mday<=last_mon_days-last_days)
-                        {
-                            cout << "The service's information: " << endl;
-                            iterator->second.display_info();
+                }
+                else {// this week devided into two months
+                    int cur_days = curr_timep->tm_mday;
+                    int last_days = 8 - cur_days;
+                    int last_mon_days = 0;
+                    int year = 1900 + curr_timep->tm_year;
+                    switch (curr_timep->tm_mon + 1) {
+                    case 1:
+                        last_mon_days = 31;
+                        break;
+                    case 2:
+                        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                            last_mon_days = 29;
                         }
+                        else
+                            last_days = 28;
+                        break;
+                    case 3:
+                        last_mon_days = 31;
+                        break;
+                    case 4:
+                        last_mon_days = 30;
+                        break;
+                    case 5:
+                        last_mon_days = 31;
+                    case 6:
+                        last_mon_days = 30;
+                        break;
+                    case 7:
+                        last_mon_days = 31;
+                        break;
+                    case 8:
+                        last_mon_days = 31;
+                        break;
+                    case 9:
+                        last_mon_days = 30;
+                        break;
+                    case 10:
+                        last_mon_days = 31;
+                        break;
+                    case 11:
+                        last_mon_days = 30;
+                        break;
+                    case 12:
+                        last_mon_days = 31;
+                        break;
+                    }
+                    if (tem_timep->tm_mon == curr_timep->tm_mon && tem_timep->tm_mday <= cur_days) {
+                        cout << "The service's information: " << endl;
+                        iterator->second.display_info();
+                    }
+                    else if (tem_timep->tm_mon == curr_timep->tm_mon - 1 && tem_timep->tm_mday <= last_mon_days - last_days)
+                    {
+                        cout << "The service's information: " << endl;
+                        iterator->second.display_info();
+                    }
+                }
+                iterator++;
             }
-           iterator++;
+            submitted = true;
+        }
+        else {
+            cout << "Today is not Friday, so the system does not need to run a weekly report." << endl;
         }
     }
-    else{
-        cout<<"Today is not Friday, so the system does not need to run a weekly report."<<endl;
-    }
+    else
+        cout << "Weekly report has been submitted, please do not repeat it. " << endl;
    return 0;
 }
 
