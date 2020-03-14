1fc839ed1c62e076f8805b93ad30048940967ff5
Thomas Pollard <tnp94@yahoo.com>
2020-03-13 17:29:05 -0700

    fixed main.cpp input problem

--- src/main.cpp
+++ src/main.cpp
@@ -61,8 +61,11 @@ int main() {
 
          default:
             choice = 0;
-            cin.clear();
-            cin.ignore();
+            if (cin.fail())
+            {
+              cin.clear();
+              cin.ignore();
+            }
             cout << "You did not select a valid response\n\n";
       }
    }
